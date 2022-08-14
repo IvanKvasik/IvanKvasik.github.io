b2cce62f15a83c957ceeaa14e2b5a5eba7b22200
Kvasik Ivan <64497012+IvanKvasik@users.noreply.github.com>
2022-08-14 13:17:37 +0300

    Update script.js

--- cat-energy/js/script.js
+++ cat-energy/js/script.js
@@ -1,16 +1,4 @@
 var menu = document.querySelector('.mobile-menu');
 function showMenu(){
-	if(menu.style.height == 0 || menu.style.height == "0px"){
-		menu.style.height = "300px";
-	}else{
-		menu.style.height = "0";
-	}
+	menu.classList.toggle('menu-opened');
 }
-/*window.onscroll = function(){
-	if(window.innerWidth <= 728){
-		var header = document.querySelector('header');
-		if(document.documentElement.scrollTop >= header.offsetHeight / 2){
-			menu.style.top = header.offsetHeight + 'px';
-		}
-	}
-};*/
